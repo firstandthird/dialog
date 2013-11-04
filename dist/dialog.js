ffa3014704e9e115bf506c3e284bf411b5ac7241
Antonio Laguna Matías <antonio@potatolondon.com>
2013-11-04 19:52:50 +0000

    Initial commit with functionality
    #1

--- dist/dialog.js
+++ dist/dialog.js
@@ -196,4 +196,119 @@
 
 })(jQuery);
 
-//dialog.js
+/*!
+ * overlay - Modal plugin
+ * v0.2.0
+ * https://github.com/firstandthird/overlay
+ * copyright First + Third 2013
+ * MIT License
+*/
+
+(function($) {
+  $.declare('overlay', {
+    defaults: {
+      overlayClass: 'overlay',
+      backdropClass: 'overlay-backdrop',
+      backdropClick: true
+    },
+
+    init: function() {
+
+      if ($('.'+this.overlayClass).length !== 0) {
+        $('.'+this.overlayClass).overlay('hide');
+      }
+
+      this.show();
+    },
+
+    getBackdrop: function() {
+      return $('.'+this.backdropClass);
+    },
+
+    showBackdrop: function() {
+      this.hideBackdrop();
+      var el = $('<div/>').addClass(this.backdropClass);
+      if (this.backdropClick) {
+        el.on('click', this.proxy(this.hide));
+      }
+      $('body').append(el);
+    },
+
+    hideBackdrop: function() {
+      this.getBackdrop().remove();
+    },
+
+    show: function() {
+      this.el.addClass(this.overlayClass);
+      $('body').css('overflow', 'hidden');
+      this.showBackdrop();
+      this.el.show();
+      this.emit('show');
+    },
+
+    hide: function() {
+      this.el.removeClass(this.overlayClass);
+      $('body').css('overflow', '');
+      this.hideBackdrop();
+      this.el.hide();
+      this.emit('hide');
+      this.el.removeData('overlay');
+    }
+  });
+})(jQuery);
+
+(function($){
+	$.declare('dialog',{
+		defaults : {
+			title: '',
+			description: '',
+			okButton: 'OK',
+			cancelButton: 'Cancel',
+			okButtonClass: '',
+			cancelButtonClass: '',
+      dialogClass: 'fidel-dialog',
+			ok: $.noop,
+			cancel: $.noop
+		},
+    init : function(){
+      var currentDialog = this.el.find('.' + this.dialogClass);
+      if (currentDialog.length !== 0){
+        this._hide();
+      }
+
+      this._createMarkup();
+      this._bindEvents();
+    },
+    _createMarkup : function(){
+      var markup = '<div class="'+ this.dialogClass +'">' +
+                      '<div class="fidel-dialog-title">' + this.title + '</div>' +
+                      '<div class="fidel-dialog-content">' + this.description + '</div>' +
+                      '<div class="fidel-dialog-buttons">' +
+                        '<button data-action="hide" data-button="ok" class="'+ this.okButtonClass +'">' + this.okButton + '</button>' +
+                        '<button data-action="hide" data-button="cancel" class="'+ this.cancelButtonClass +'">' + this.cancelButton + '</button>' +
+                      '</div>'+
+                    '</div>';
+      this.dialog = $(markup);
+      this.overlay = this.dialog.overlay({ backdropClick: false });
+      this.el.append(this.dialog);
+    },
+    _bindEvents : function(){
+      this.dialog.find('button').on('click', this.proxy(this._onButtonClick));
+    },
+    _hide : function(){
+      this.overlay.overlay('hide');
+      this.el.removeData('dialog');
+      this.dialog.remove();
+    },
+    _onButtonClick : function(e){
+      e.preventDefault();
+      e.stopImmediatePropagation();
+      var element = $(e.currentTarget || e.toElement),
+          button = element.data('button');
+
+      this[button].call();
+      this._hide();
+    }
+
+	});
+})(jQuery);
--- test/dialog.test.js
+++ test/dialog.test.js
@@ -1,4 +1,55 @@
-
 suite('dialog', function() {
+  var $body = $('body');
 
+  setup(function(){
+    $body.dialog({
+      title : 'Do you want some coffee?',
+      description : 'You should definitely try to get some good coffee',
+      okButton : 'Yes',
+      cancelButton : 'No'
+    });
+  });
+
+  test('$().dialog exists',function(){
+    assert.equal(typeof $('body').dialog, 'function');
+  });
+
+  test('dialog creates an overlay', function(){
+    assert.equal($('.fidel-dialog').length,1);
+  });
+
+  test('dialog has a title with passed text', function(){
+    assert.equal($('.fidel-dialog-title').text(),'Do you want some coffee?');
+  });
+
+  test('dialog has a body with passed text', function(){
+    assert.equal($('.fidel-dialog-content').text(),'You should definitely try to get some good coffee');
+  });
+
+  test('dialog has an ok button with passed text', function(){
+    assert.equal($('.fidel-dialog').find('button').first().text(),'Yes');
+  });
+
+  test('dialog has an cancel button with passed text', function(){
+    assert.equal($('.fidel-dialog').find('button').last().text(),'No');
+  });
+
+  test('dialog should fire a function when OK is pressed', function(done){
+    $body.dialog('_hide');
+    $body.dialog({
+      ok : function(){
+        done();
+      }
+    });
+    $('.fidel-dialog').find('button').first().click();
+  });
+  test('dialog should fire a function when Cancel is pressed', function(done){
+    $body.dialog('_hide');
+    $body.dialog({
+      cancel : function(){
+        done();
+      }
+    });
+    $('.fidel-dialog').find('button').last().click();
+  });
 });
